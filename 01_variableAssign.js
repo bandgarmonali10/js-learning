881a8e3f2b3efa1bb6b3ff5833829c6589f2f49a
bandgarmonali10 <bandgarmonali058@gmail.com>
2024-03-08 16:03:39 +0530

    JS Assign 01
    JS Assign 01

---
+++ 01_variableAssign.js
@@ -0,0 +1,27 @@
+console.log("==========01==========");
+var myFullName;
+console.log("My Full Name variable value before Initialization:",myFullName);
+console.log("My Full Name varible value after Initialization:Monali Bandgar");
+console.log("My full Name variable value after updation:Monali Narayan Bandgar");
+
+console.log("==========02==========");
+myLove = "I love only JS"
+console.log("I love only JS");
+
+console.log("==========03==========");
+firstName = "Monali";
+console.log("First name:", firstName);
+lastName = "Bandgar";
+console.log("Last Name:", lastName);
+age = "123";
+console.log("Age:", age);
+address = "xyz";
+console.log("Address:", address);
+pinCode = "123456";
+console.log("Pincode:", pinCode);
+gender = "Female";
+console.log("Gender:", gender);
+isMarried = "false";
+console.log("Is Married:", isMarried);
+
+
