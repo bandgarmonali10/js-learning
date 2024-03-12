780763de38fc5d6b76bdf63a8fecabb40fe0db2d
bandgarmonali10 <bandgarmonali058@gmail.com>
2024-03-12 13:03:26 +0530

    03_funAssig02.js
    03_funAssig02.js

--- 03_funAssig02.js
+++ 03_funAssig02.js
@@ -12,4 +12,4 @@ bankDetails("CITI Bank",3456782345,'Pune',431202);
 console.log("*************************************");
  bankDetails("Axis Bank",7856782345,'Mumbai',441202);
  console.log("************************************");
- bankDetails("HDFC Bank",8956782345,'Pune',631202, "Open");
+ bankDetails("HDFC Bank",8956782345,'Pune',631202, "Open"); 
