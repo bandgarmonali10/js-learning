9d920746f5c80de39d18d6ddeb194d01aab60174
bandgarmonali10 <bandgarmonali058@gmail.com>
2024-03-12 14:59:55 +0530

    03_stringBasicsAssig01.js
    03_stringBasicsAssig01.js

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,22 @@
+
+
+function stringBasics(){
+    var company = "Infosys";
+    console.log(`My dream company is: ${company}`);
+}
+stringBasics();
+
+function myhobbies(){
+ var Yourhobby1="Learning";
+ var Yourhobby2="Photography";
+ var Yourhobby3="Tourism";
+
+var result = Yourhobby1+Yourhobby2 +Yourhobby3;
+
+ console.log(`My Hobbies Are: ${Yourhobby1} ${Yourhobby2} ${Yourhobby3}`);
+var totalChars=result.length;
+
+console.log(`Total number of chars : ${totalChars}`);
+}
+myhobbies();
+
