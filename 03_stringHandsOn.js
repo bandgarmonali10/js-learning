8c7f55ecc56ae5ba824dccc5a57770fb67ae2159
bandgarmonali10 <bandgarmonali058@gmail.com>
2024-03-14 14:34:23 +0530

    05_operatorA.js
    05_operatorA.js

--- 03_stringHandsOn.js
+++ 03_stringHandsOn.js
@@ -1,3 +1,5 @@
+
+
 function stringHandsOn(){
     var greet = "   Hey you are doing good, keep it up   ";
    console.log("=====Step 1=====");
@@ -25,18 +27,18 @@ console.log("=====Step 4======");
    console.log(`First char after trim is: ${greet.charAt(0)} Last char after trim is: ${greet.charAt(totalChars-1)}`);
    console.log("******************************************************");
    console.log("=====Step 6======");
-var greet = "Hey you are doing good, keep it up";
+
 var resultValue = greet.split(" ")
 console.log(resultValue);
 console.log(`Total words available in the string are: ${resultValue.length}`);
 console.log("******************************************************");
+
  console.log("========Step 7=======");
- var greet = "Hey you are doing good, keep it up";
 var result = greet.search("good");
 console.log(`'good' is available at ${result}`);
 console.log("******************************************************");
 console.log("======Step 8=====");
-var greet = "Hey you are doing good, keep it up";
+
 var result=greet.substring(22);
 console.log(`substring starting from index 22 is: ${greet.substring(22)}`);
 console.log("******************************************************");
@@ -46,7 +48,7 @@ var result=greet.includes("");
 console.log(`Is string ends with word "up": ${result}`);
 console.log("******************************************************");
 console.log("======Step 10======");
-var greet = "Hey you are doing good, keep it up";
+
 var result = greet.includes("");
 console.log(`Is string starts with word "Hey": ${result}`);
 }
---
+++ 05_operatorA.js
@@ -0,0 +1,61 @@
+
+ function squareOfWordLength(){
+  
+ }
+ console.log("**************************Step 1******************************");
+  squareOfWordLength("JavaScript");
+  var word = "JavaScript";
+  var length = word.length;
+var n1 = 10;
+var n2 = 2;
+var result = n1 ** n2;
+console.log(`Length square of ${word} is: ${result}`);
+
+  squareOfWordLength("Google Chrome");
+   var word = "Google Chrome";
+   var length = word.length;
+ var n1 = 13;
+ var n2 = 2;
+ var result = n1 ** n2;
+console.log(`Length square of ${word} is: ${result}`);
+
+  squareOfWordLength("Developer Smart");
+   var word = "Developer Smart";
+   var length = word.length;
+ var n1 = 15;
+ var n2 = 2;
+ var result = n1 ** n2;
+console.log(`Length square of ${word} is: ${result}`);
+
+console.log("**************************Step 2******************************");
+
+function totalWords(){
+    
+}
+
+var sentence = "I am Angular Developer";
+    var result = sentence.length;
+    // console.log(`Length of ${sentence} : ${result}`);
+
+     resultValue = sentence.split(" ")
+// console.log(resultValue);
+// console.log(`Total words available in the string are: ${resultValue.length}`);
+
+var n1 = 22;
+var n2 = 4;
+var result = 22 / 4;
+console.log(` Division after String length divide by total no.of words is:${result}`);
+ totalWords(resultValue);
+
+ 
+ var n1 = 22;
+ var n2 = 4;
+ var result = 22 * 4;
+ console.log(` Multiplication after String length multiplied by total no.of words is:${result}`);
+  totalWords(resultValue);
+ 
+
+
+
+
+ 
