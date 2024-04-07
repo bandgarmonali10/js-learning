1e5563e91cd4ce8515c3581a280ae1e58564970b
bandgarmonali10 <bandgarmonali058@gmail.com>
2024-04-07 17:59:03 +0530

    10_jsonAssign.js
    10_jsonAssign.js

---
+++ 06_primeNumbersAssign.js
@@ -0,0 +1,72 @@
+
+
+const array = [3,9,7,6,19,29,53]
+ function isPrime(num){
+    for (let index = 2; index < num; index++) {
+      if (num%index==0) {
+       return false;
+       } 
+     }
+     return true;
+    }
+
+   
+    const result = isPrime(3);
+    console.log(`Is 3 Prime Number : ${result}`);
+   
+    const result1 = isPrime(9);
+    console.log(`Is 9 Prime Number : ${result1}`);
+
+   const result2 = isPrime(7);
+   console.log(`Is 7 Prime Number : ${result2}`);
+   
+    const result3 = isPrime(6);
+    console.log(`Is 6 Prime Number : ${result3}`);
+
+    const result4 = isPrime(19);
+    console.log(`Is 19 Prime Number : ${result4}`);
+   
+    const result5 = isPrime(29);
+    console.log(`Is 29 Prime Number : ${result5}`);
+
+   const result6 = isPrime(53);
+    console.log(`Is 53 Prime Number : ${result6}`);
+
+
+
+   function countNumber(arr){
+let count = 0;
+ let primeArray = [];
+ arr.forEach(element => {
+  if (isPrime(element)) {
+     count++;
+    primeArray.push(element);
+  }
+ });
+// console.log(`Prime Numbers : ${primeArray}`);
+console.log(`Count of prime numbers : ${count}`);
+   }
+countNumber(array)
+
+   console.log(`============Step 02===========`);
+   function spaceCount(str) {
+  
+    let count = 0;
+  
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === ' ') {
+            count++;
+        }
+    }
+  
+    return count;
+  }
+  
+  
+  const string1 = "Revision is the mother of success";
+  const spaces1 = spaceCount(string1);
+  console.log("Total number of spaces in string1 =>", spaces1);
+  
+  const string2 = "java script is language of internate world";
+  const spaces2 = spaceCount(string2);
+  console.log("Total number of spaces in string2 =>", spaces2);
---
+++ 10_jsonAssign.js
@@ -0,0 +1,31 @@
+
+const jsonStr =`{
+    "name" : "Aleix Melon",
+    "id" : "E00245",
+    "role" :["Dev", "DBA"],
+    "age" : 23,
+    "doj" : "11-12-2019",
+    "married" : false,
+    "address" : {
+        "street" : "32, Laham St.",
+        "city" : "Innsbruck",
+        "country" : "Austria"
+    },
+    "referred-by" : "E0012"
+
+}`
+console.log(`======== Convert JSON to Object ===========`);
+const obj = JSON.parse(jsonStr);
+console.log(obj);
+
+const array = ["Dev","DBA"];
+const result = array.splice(0,1);
+console.log(`role => ${result}`);
+
+const arr = ["Aleix","Melon"];
+const result1 = arr.pop();
+console.log(`Last name => ${result1}`);
+
+const arr1 = [11,12,2019];
+const result2 = arr1.pop();
+console.log(`Joining year of employee => ${result2}`);
